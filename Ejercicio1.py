e3f6dbfc9a5d4b4374b2d772e4a54f4a95904d0f
Alexis Viglianti <148451612+Andext@users.noreply.github.com>
2024-05-08 08:39:12 -0300

    Update Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -12,7 +12,7 @@ print("Valor de la variable booleano:", booleano)
 
 
 # Imprimir valores de las variables
-print("Número entero:", numero_entero)
-print("Número flotante:", numero_flotante)
+print("Número entero:", numero_entero) # type: ignore
+print("Número flotante:", numero_flotante) # type: ignore
 print("Cadena:, cadena")
-print("¿Es verdadero?", es_verdadero)
+print("¿Es verdadero?", es_verdadero) # type: ignore
