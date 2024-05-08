151390f7d7539f34a5ec3d8e1ec015930b711d3d
Alexis Viglianti <148451612+Andext@users.noreply.github.com>
2024-05-08 08:29:42 -0300

    Update and rename # Ejercicios de Python.py to # Ejercicio 2.py

---
+++ # Ejercicio 2.py
@@ -0,0 +1,33 @@
+# Declaración de variables numéricas
+num1 = 10
+num2 = 5
+
+# Operaciones aritméticas
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+modulo = num1 % num2
+
+# Impresión de resultados
+print("Resultado de la suma:", suma)
+print("Resultado de la resta:", resta)
+print("Resultado de la multiplicación:", multiplicacion)
+print("Resultado de la división:", division)
+print("Resultado del módulo:", modulo)
+
+# Comparaciones
+igualdad = num1 == num2
+diferencia = num1 != num2
+mayor_que = num1 > num2
+menor_que = num1 < num2
+mayor_o_igual_que = num1 >= num2
+menor_o_igual_que = num1 <= num2
+
+# Impresión de comparaciones
+print("¿num1 es igual a num2?", igualdad)
+print("¿num1 es diferente de num2?", diferencia)
+print("¿num1 es mayor que num2?", mayor_que)
+print("¿num1 es menor que num2?", menor_que)
+print("¿num1 es mayor o igual que num2?", mayor_o_igual_que)
+print("¿num1 es menor o igual que num2?", menor_o_igual_que)
