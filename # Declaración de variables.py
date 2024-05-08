f3e4d0b50404c05febece0cc79f4d77dfc63920f
Alexis Viglianti <148451612+Andext@users.noreply.github.com>
2024-05-08 08:25:38 -0300

    # Declaración de variables.py

---
+++ # Declaración de variables.py
@@ -0,0 +1,18 @@
+# Declaración de variables
+entero = 10
+flotante = 3.14
+cadena = "Hola, mundo!"
+booleano = True
+
+# Impresión de valores
+print("Valor de la variable entero:", entero)
+print("Valor de la variable flotante:", flotante)
+print("Valor de la variable cadena:", cadena)
+print("Valor de la variable booleano:", booleano)
+
+
+# Imprimir valores de las variables
+print("Número entero:", numero_entero)
+print("Número flotante:", numero_flotante)
+print("Cadena:, cadena")
+print("¿Es verdadero?", es_verdadero)
