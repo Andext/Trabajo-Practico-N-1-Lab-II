aac17fb53e9b89ba8a59a014511f0571d8caf606
Alexis Viglianti <148451612+Andext@users.noreply.github.com>
2024-05-10 04:25:48 -0300

    Actualizar Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,17 +1,14 @@
-# Declaración de variables
 entero = 10
 flotante = 3.14
 cadena = "Hola, gente!"
 es_verdadero = True
 
-# Impresión de valores
 print("Valor de la variable entero:", entero)
 print("Valor de la variable flotante:", flotante)
 print("Valor de la variable cadena:", cadena)
 print("Valor de la variable booleano:", es_verdadero)
 
 
-# Imprimir valores de las variables
 print("Número entero:", entero)
 print("Número flotante:", flotante) 
 print("Cadena:, cadena")
