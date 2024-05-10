99dfd0771a05c8cfa80c56da1d061502a047a8f9
Alexis Viglianti <148451612+Andext@users.noreply.github.com>
2024-05-10 04:25:11 -0300

    Actualizar Ejercicio 2.py

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -1,22 +1,18 @@
-# Declaración de variables numéricas
 num1 = 10
 num2 = 5
 
-# Operaciones aritméticas
 suma = num1 + num2
 resta = num1 - num2
 multiplicacion = num1 * num2
 division = num1 / num2
 modulo = num1 % num2
 
-# Impresión de resultados
 print("Resultado de la suma:", suma)
 print("Resultado de la resta:", resta)
 print("Resultado de la multiplicación:", multiplicacion)
 print("Resultado de la división:", division)
 print("Resultado del módulo:", modulo)
 
-# Comparaciones
 igualdad = num1 == num2
 diferencia = num1 != num2
 mayor_que = num1 > num2
@@ -24,7 +20,6 @@ menor_que = num1 < num2
 mayor_o_igual_que = num1 >= num2
 menor_o_igual_que = num1 <= num2
 
-# Impresión de comparaciones
 print("¿num1 es igual a num2?", igualdad)
 print("¿num1 es diferente de num2?", diferencia)
 print("¿num1 es mayor que num2?", mayor_que)
