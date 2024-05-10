98043532d87cd83a90ca44144d547d9d7cb460b4
Alexis Viglianti <148451612+Andext@users.noreply.github.com>
2024-05-10 04:21:48 -0300

    Actualizar Ejercicio 7.py

--- Ejercicio 7.py
+++ Ejercicio 7.py
@@ -8,7 +8,6 @@ radio1 = 5
 radio2 = 10
 radio3 = 2.5
 
-# Muestra los resultados
 print("El área de un círculo con radio", radio1, "es:", calcular_area_circulo(radio1))
 print("El área de un círculo con radio", radio2, "es:", calcular_area_circulo(radio2))
 print("El área de un círculo con radio", radio3, "es:", calcular_area_circulo(radio3))
