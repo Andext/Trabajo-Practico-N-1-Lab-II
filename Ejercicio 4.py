282edeb5dd225186369545e291d76c24e0e4fef0
Alexis Viglianti <148451612+Andext@users.noreply.github.com>
2024-05-10 04:23:48 -0300

    Actualizar Ejercicio 4.py

--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -1,5 +1,3 @@
-# Define una tupla llamada colores que contenga los colores 'rojo', 'verde' y 'azul'.
 colores = ('rojo', 'verde', 'azul')
 
-# Imprime el segundo color de la tupla.
 print("El segundo color de la tupla es:", colores[1])
